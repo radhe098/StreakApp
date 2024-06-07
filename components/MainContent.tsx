d8636662c528bf6d2bdcb49302ce46b8f6fb2f80
[radhe098] <[purushottamtapase1@gmail.com]>
2024-06-07 23:43:39 +0530

    ready to go

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -2,7 +2,7 @@ export default function mainComponent(){
     return(
         <div className="w-[80%]] h-[80%] flex justify-center m-12 border-white border-8 ">
             
-            <iframe width="1200" height="800" src="https://www.youtube.com/embed/STScKOUpXR8?si=kDdVotD-cShFgyZs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="1200" height="800" src="https://www.youtube.com/embed/STScKOUpXR8?si=kDdVotD-cShFgyZs" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  ></iframe>
             
         </div>
     );
